ab1b39bc18dac6b28511edba0c5f2abd67529184
Mathunder <matthieu.dhoop@gmail.com>
2025-01-16 16:36:36 +0100

    component functionnal

--- image_upload_component/src/app/ui/upload-image.tsx
+++ image_upload_component/src/app/ui/upload-image.tsx
@@ -4,6 +4,7 @@ import { useActionState, useState } from "react";
 import { uploadFile } from "../actions/upload-image";
 import { PhotoIcon, XCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import clsx from "clsx";
 
 export default function FileUploadForm() {
   const [state, formAction] = useActionState(uploadFile, {
@@ -25,19 +26,31 @@ export default function FileUploadForm() {
     }
   };
 
-  const handleDrag = (event: React.DragEvent<HTMLDivElement>) => {
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    if (event.type === "dragenter" || event.type === "dragover") {
-      setDragActive(true);
-    } else if (event.type === "dragleave") {
-      setDragActive(false);
-    }
+    setDragActive(true);
+  };
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragActive(false);
   };
 
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    handleFileSelect(file);
+    setDragActive(false);
+    const files = event.dataTransfer.files;
+    if (files.length > 0) {
+      handleFileSelect(files[0]);
+
+      // One needs to update the input's selected file because the drag and drop do not affect the input's selected file
+      const dataTransfer = new DataTransfer();
+      dataTransfer.items.add(files[0]);
+      const inputElement = document.getElementById("file") as HTMLInputElement;
+      if (inputElement) {
+        inputElement.files = dataTransfer.files;
+      }
+    }
   };
 
   const handleDeleteUpload = () => {
@@ -46,23 +59,33 @@ export default function FileUploadForm() {
     handleFileSelect(null);
   };
 
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      handleFileSelect(files[0]);
+    }
+  };
+
   return (
     <div className="max-w-md mx-auto p-4 space-y-4">
       <form action={formAction} className="space-y-4">
-        {!previewUrl ? (
-          <div
-            onDragOver={handleDrag}
-            onDragEnter={handleDrag}
-            onDragLeave={handleDrag}
-            onDrop={handleDrop}
-            className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
-          >
-            <div className="text-center">
-              <PhotoIcon
-                aria-hidden="true"
-                className="mx-auto size-12 text-gray-300"
-              />
-              <div className="mt-4 flex text-sm/6 text-gray-600">
+        <div
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          className={clsx(
+            "flex flex-col justify-center items-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10",
+            {
+              "bg-indigo-50": dragActive === true,
+            }
+          )}
+        >
+          <div className="text-center">
+            <PhotoIcon
+              aria-hidden="true"
+              className="mx-auto size-12 text-gray-300"
+            />
+            <div className="mt-4 flex text-sm/6 text-gray-600">
               <label
                 htmlFor="file"
                 className="relative cursor-pointer rounded-lg font-semibold text-indigo-600"
@@ -72,15 +95,16 @@ export default function FileUploadForm() {
                   type="file"
                   id="file"
                   name="file"
+                  onChange={handleFileChange}
                   accept=".jpg,.png,.gif"
-                  className="sr-only"
+                  className="hidden"
                 />
               </label>
-              <p className="pl-1">your JPEG file.</p></div>
+              <p className="pl-1">your JPEG file.</p>
             </div>
           </div>
-            ) : (
-            <div className="relative rounded-lg border border-dashed border-gray-900/25 px-3 py-5">
+          {previewUrl && (
+            <div className="relative">
               <div className="absolute top-2 right-1 cursor-pointer">
                 <XCircleIcon
                   onClick={handleDeleteUpload}
@@ -88,7 +112,6 @@ export default function FileUploadForm() {
                   className="size-6 "
                 />
               </div>
-
               <Image
                 src={previewUrl}
                 alt="Preview"
@@ -96,14 +119,22 @@ export default function FileUploadForm() {
                 height={100}
                 className="object-cover rounded-lg w-full"
               />
+              <p className="text-gray-400 text-sm mt-4">
+                {uploadedFile
+                  ? "File size : " + uploadedFile.size + " bytes"
+                  : ""}
+              </p>
             </div>
-            )}
+          )}
+        </div>
+
         {state.errors?.file && (
-                <p className="text-red-500 text-sm mt-1">
-                  {state.errors.file[0]}
-                </p>
-              )}
-        <button type="submit" className="w-full bg-indigo-600 text-white py-1 px-3 rounded-md">
+          <p className="text-red-500 text-sm mt-1">{state.errors.file[0]}</p>
+        )}
+        <button
+          type="submit"
+          className="w-full bg-indigo-600 text-white py-1 px-3 rounded-md"
+        >
           Upload File
         </button>
       </form>
